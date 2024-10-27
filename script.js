ed5013b4fed5e3692645bad9fa5d79cda2bacf79
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-10-27 08:50:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+const square = document.querySelectorAll('.square');
+
+square.forEach((sq) => {
+    sq.addEventListener("mouseenter", () => {
+        changedHoveredColor(sq);
+    })
+
+    sq.addEventListener("mouseleave", restcolor);
+})
+
+ function changedHoveredColor(hoveredSquare) {
+    const square = document.querySelectorAll('.square');
+    square.forEach((sq) => {
+        if(hoveredSquare !== sq){
+            sq.style.backgroundColor = '#6F4E37';
+        }
+    })
+ }
+
+ function restcolor(){
+     const square = document.querySelectorAll('.square');
+    square.forEach((sq) => {
+        sq.style.backgroundColor = '#E6E6FA';
+    })
+ }
